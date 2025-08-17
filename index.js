34dfef5883983f325cf5905ec270ffad01fa3cbc
rk-tanvir790 <ktanvir790@gmail.com>
2025-08-17 23:09:13 +0600

    created API for find single coffee

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express();
 
@@ -31,6 +31,13 @@ async function run() {
         // Api for get coffee
         app.get('/coffees', async (req, res) => {
             const result = await coffeeCollections.find().toArray();
+            res.send(result);
+        })
+        // Api for one item get coffee
+        app.get('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const result = await coffeeCollections.findOne(filter);
             res.send(result);
         })
         // Api for add coffee
